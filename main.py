8fb5148033ae5a44bd71485644a4805e70779413
fregogui <guillaume.fregosi@gmail.com>
2019-10-19 16:21:21 +0200

    add front brake and rotors

--- main.py
+++ main.py
@@ -1,47 +1,70 @@
-import csv
+import pandas as pd
+import argparse
 
-vehicle ="77752"
-intervention="front_brake_pads"
-
-##Parse article_vehicle_relations file and store the result in a dict. It is not necessary if we need only one request but it will be helpfull to extend 
-
-articleByVehicle={}
-
-with open('./data/article_vehicle_relations.csv', newline='') as csvfile:
-  reader = csv.reader(csvfile, delimiter=',')
-  next(reader, None)  # skip the headers
-  for row in reader:
-    if row[0] not in articleByVehicle: #attention au titre
-      articleByVehicle[row[0]]=set()
-    articleByVehicle[row[0]].add(row[1])
-
-articleByType={}
-with open('./data/articles.csv', newline='') as csvfile:
-  reader = csv.reader(csvfile, delimiter=',')
-  next(reader, None)  # skip the headers
-  for row in reader:
-    if row[0] not in articleByType:
-      articleByType[row[0]]=set()
-    articleByType[row[0]].add(row[2])
-
-articlesByIntervention={
-  "front_brake_pads": {
-    "brake_pad":4
-  },
-  "front_brake_rotors_and_pads":{
-    "brake_pad":4,
-    "brake_rotor":2
-  },
-  "oil_change":{},
-  "injectors":{}
-}
+def compute_price(vehicle, intervention):
+    if intervention == "front_brake_pads":
+        return compute_price_front_brake_pads(vehicle)
+    if intervention == "front_brake_rotors_and_pads":
+        return compute_price_front_brake_pads(vehicle
 
 
+def compute_price_front_brake_pads(vehicle):
+    article_ids = get_article_ids(vehicle, "brake_pad")
+    return 4 * get_median_price(article_ids, 4)
+
+def compute_price_front_brake_rotors_and_pads(vehicle):
+    article_ids_rotors = get_article_ids(vehicle, "brake_rotor")
+    article_ids_brake_pad = get_article_ids(vehicle, "brake_pad")
+    return 2 * get_median_price(article_ids_rotors, 2) + 4 * get_median_price(article_ids_brake_pad, 4)
+
+def get_article_ids(vehicle, type):
+    articles_compatible = relations[relations["vehicle_id"] == vehicle]
+    articles_compatible = pd.merge(articles_compatible, articles, left_on="article_id", right_on="id")
+    articles_compatible = articles_compatible[articles_compatible["type"] == type]
+    return list(articles_compatible["id"])
+
+def get_median_price(article_ids, min_available):
+    print(article_ids)
+    valid_references = catalog[(catalog["quantity"].astype(int) >= min_available) & (catalog["article_id"].isin(article_ids))]
+    median_price = valid_references["price"].median()
+    return median_price
+    #prendre en compte pas de dispo
+
+def parse_all_catalogs():
+    catalog_1 = pd.read_csv("./data/catalogs/auto_distribution.csv", decimal=",")
+    catalog_1.rename(columns={
+        "Quantity": "quantity",
+        "Price": "price",
+        "Part number": "article_id",
+    }, inplace=True)
+    catalog_2 = pd.read_csv("./data/catalogs/acr.csv", decimal=".", delimiter=";")
+    catalog_2.rename(columns={
+        "qté": "quantity",
+        "prix total HT": "price",
+        "ref article": "article_id",
+    }, inplace=True)
+
+    catalog_3 = pd.read_csv("./data/catalogs/golda.csv")
+    catalog_3.rename(columns={
+        "Quantité": "quantity",
+        "Prix total": "price",
+        "Référence pièce": "article_id",
+    }, inplace=True)
+    return pd.concat([catalog_1, catalog_2, catalog_3])
+
+if __name__ == "__main__":
+
+    vehicles = pd.read_csv("./data/vehicles.csv")
+    articles = pd.read_csv("./data/articles.csv")
+    relations = pd.read_csv("./data/article_vehicle_relations.csv")
+
+    catalog = parse_all_catalogs()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--intervention')
+    parser.add_argument('--vehicle', type=int)
+    args = parser.parse_args()
 
 
-def getPrice(intervention,vehicle):
-  print(articleByType["brake_pad"].intersection(articleByVehicle[vehicle]))
-
-getPrice(intervention,vehicle)
-
-#questions: oil filter = fuel filter ?
+    price = compute_price(args.vehicle, args.intervention)
+    print("Le prix : {}".format(str(price)))
---
+++ main2.py
@@ -0,0 +1,47 @@
+import csv
+
+vehicle ="77752"
+intervention="front_brake_pads"
+
+##Parse article_vehicle_relations file and store the result in a dict. It is not necessary if we need only one request but it will be helpfull to extend
+
+articleByVehicle={}
+
+with open('./data/article_vehicle_relations.csv', newline='') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',')
+  next(reader, None)  # skip the headers
+  for row in reader:
+    if row[0] not in articleByVehicle: #attention au titre
+      articleByVehicle[row[0]]=set()
+    articleByVehicle[row[0]].add(row[1])
+
+articleByType={}
+with open('./data/articles.csv', newline='') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',')
+  next(reader, None)  # skip the headers
+  for row in reader:
+    if row[0] not in articleByType:
+      articleByType[row[0]]=set()
+    articleByType[row[0]].add(row[2])
+
+articlesByIntervention={
+  "front_brake_pads": {
+    "brake_pad":4
+  },
+  "front_brake_rotors_and_pads":{
+    "brake_pad":4,
+    "brake_rotor":2
+  },
+  "oil_change":{},
+  "injectors":{}
+}
+
+
+
+
+def getPrice(intervention,vehicle):
+  print(articleByType["brake_pad"].intersection(articleByVehicle[vehicle]))
+
+getPrice(intervention,vehicle)
+
+#questions: oil filter = fuel filter ?
